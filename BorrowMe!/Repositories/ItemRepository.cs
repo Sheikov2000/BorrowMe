945dc5c7b007270fc0ca42a3d4edbdc8bdee37e9
Nick Sheikov <sheikov2000@yahoo.com>
2023-07-25 16:44:13 -0400

    continue to set up files in client side

---
+++ BorrowMe!/Repositories/ItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using BorrowMe.Utils;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using BorrowMe.Models;
+using BorrowMe.Repositories;
+
+
+namespace BorrowMe.Repositories
+{
+    public class ItemRepository
+    {
+
+
+    }
+}
